30edec3fa2374f62015a4d6dfdfad1bd90c6e86c
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2026-03-05 14:36:00 +0100

    chore: rename dashboard.js to Dashboard.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GreenhouseDashboard from './dashboard';
+import GreenhouseDashboard from './Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<GreenhouseDashboard />);
