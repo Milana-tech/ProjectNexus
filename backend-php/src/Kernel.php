d0b4daf58155fbba75af348409abffd0f6ddbce0
CadarIustin <iustin.cadar@student.nhlstenden.com>
2026-03-19 18:06:08 +0100

    Fixing a possible conflict with main

--- backend-php/src/Kernel.php
+++ backend-php/src/Kernel.php
@@ -2,29 +2,38 @@
 
 namespace App;
 
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    public function registerBundles(): iterable
+    {
+        return [
+            new FrameworkBundle(),
+        ];
+    }
+
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->services()
-            ->defaults()
-                ->autowire()
-                ->autoconfigure();
+        $services = $container->services();
+        $services->defaults()
+            ->autowire()
+            ->autoconfigure();
 
-        $container->services()
-            ->load('App\\', '../src/')
-            ->exclude('../src/Kernel.php');
+        $services->load('App\\', dirname(__DIR__).'/src/')
+            ->exclude(dirname(__DIR__).'/src/{Kernel.php}');
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../src/Controller/', 'attribute');
+        $routes->import(dirname(__DIR__).'/src/Controller/', 'attribute');
     }
 }
